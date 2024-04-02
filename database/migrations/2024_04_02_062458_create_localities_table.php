c86f4f7b392ba2ff0ac4f5588cca3fe777ca1286
erickalonso98 <erick.edjaa@gmail.com>
2024-04-02 00:27:46 -0600

    se creo la tabla Localities con sus respectivo modelos,controladores y migracion

---
+++ database/migrations/2024_04_02_062458_create_localities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('localities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('municipalitie_id');
+            $table->timestamps();
+
+            $table->foreign('municipalitie_id')
+                  ->references('id')
+                  ->on('municipalities');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('localities');
+    }
+};
