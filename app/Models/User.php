2f77706a38fc5f74ba8d401e71f741d495db7c6a
erickalonso98 <erick.edjaa@gmail.com>
2024-04-03 16:55:13 -0600

    user y members relacionados con exito a nivel de modelos

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,8 @@ class User extends Authenticatable
     public function roles(){
         return $this->belongsToMany(Role::class);
     }
+
+    public function members(){
+        return $this->hasMany(Member::class);
+    }
 }
