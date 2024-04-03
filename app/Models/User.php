ad5db8af27995f985a38268b7b05faae3cf194b2
erickalonso98 <erick.edjaa@gmail.com>
2024-04-03 15:42:28 -0600

    se modifico el nombre del archivo del modelo Role y se hizo pruebas de relacion en los modelos User

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,9 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
+     protected $table = 'users';
+
     protected $fillable = [
         'name',
         'email',
@@ -42,4 +45,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function roles(){
+        return $this->belongsToMany(Role::class);
+    }
 }
