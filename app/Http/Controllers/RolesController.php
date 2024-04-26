4b768d271a4562b710a901fb0e426850fd84c4d7
erickalonso98 <erick.edjaa@gmail.com>
2024-04-26 17:17:15 -0600

    crud de roles creado exitosamente

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\Role;
 
@@ -16,5 +17,108 @@ class RolesController extends Controller
             "status" => "success",
             "roles"  => $roles
         ],200);
+    }
+
+    public function show($id){
+        $role = Role::find($id);
+
+        if(is_object($role) && !empty($role) && isset($role)){
+            $data = [
+                "status"  => "success",
+                "code"    => 200,
+                "role"    => $role
+            ];
+        }else{
+            $data = [
+                "status"  => "error",
+                "code"    => 404,
+                "message" => "No se encuentra el rol"
+            ];
+        }
+
+        return response()->json($data,$data["code"]);
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            "name" => "required"
+        ]);
+
+        if($validator->fails()){
+            $data = [
+                "status"  => "error",
+                "code"    => 404,
+                "message" => "El nombre del rol es requerido"
+            ];
+        }else{
+            $role = new Role();
+
+            $role->name = $request->name;
+
+            $role->save();
+
+            $data = [
+                "status"  => "success",
+                "code"    => 200,
+                "message" => "Rol creado con exito!!",
+                "role"    => $role
+            ];
+        }
+
+        return response()->json($data,$data["code"]);
+    }
+
+    public function update(Request $request,$id){
+        $role = Role::findOrFail($id);
+
+        $validator = Validator::make($request->all(),[
+            "name" => "required"
+        ]);
+
+        if($validator->fails()){
+            $data = [
+                "status"  => "error",
+                "code"    => 404,
+                "message" => "El nombre del rol es requerido"
+            ];
+        }else{
+
+            $role->name = $request->name;
+
+            $role->save();
+
+            $data = [
+                "status"  => "success",
+                "code"    => 200,
+                "message" => "Rol actualizado con exito!!",
+                "role"    => $role
+            ];            
+        }
+
+        return response()->json($data,$data["code"]);
+    }
+
+    public function destroy($id){
+        $role = Role::find($id);
+
+        if(is_object($role) && !empty($role) && isset($role)){
+
+            $role->delete();
+
+            $data = [
+                "status"  => "success",
+                "code"    => 200,
+                "message" => "Rol eliminado",
+                "role"    => $role
+            ];
+        }else{
+            $data = [
+                "status"  => "error",
+                "code"    => 404,
+                "message" => "No se encuentra el rol"
+            ];
+        }
+
+        return response()->json($data,$data["code"]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,20 +20,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post("/api/register",[UserController::class,"store"]);
+
 Route::post("/api/login/",[UserController::class,"login"]);
 
 Route::middleware("api.auth")->group(function(){
-    // Rutas de Usuario
+    // *Rutas de Usuario
     Route::get("/api/users",[UserController::class,"index"]);
     Route::get("/api/user/{id}",[UserController::class,"show"]);
+    Route::post("/api/register",[UserController::class,"store"]);
     Route::delete("/api/delete-user/{id}",[UserController::class,"destroy"]);
     Route::post("/api/user/upload",[UserController::class,"upload"]);
     Route::get("/api/user/avatar/{filename}",[UserController::class,"getImage"]);
     Route::put("/api/modify-user/{id}",[UserController::class,"update"]);
 
-    //Rutas de roles
+    // * Rutas de roles
     Route::get("/api/roles-users",[RolesController::class,"index"]);
+    Route::get("/api/role-user/{id}",[RolesController::class,"show"]);
+    Route::post("/api/register-roles",[RolesController::class,"store"]);
+    Route::put("/api/modify-role/{id}",[RolesController::class,"update"]);
+    Route::delete("/api/delete-role/{id}",[RolesController::class,"destroy"]);
+
+    //* Rutas de Permisos
 });
 
 
